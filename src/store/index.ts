0b8b37b34d82308f14d1aca9a9f90d8fa9ba7442
SamueLVitor1 <samuelvitor668@gmail.com>
2026-03-30 10:26:20 -0300

    feat: implementação do tailwindcss

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,10 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 
-const todoSlice = createSlice({
-  name: "todo",
-  initialState: ["Fazer café", "Lavar a louça"],
-
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
